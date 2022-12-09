f98ad29ff3bf7357f4eb11e1733f70105ba8735e
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-09 22:02:12 +0200

    init

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+
+export default {
+  input: "custom-templates/image-grids/flex-dest/dev/js/index.js",
+  output: {
+    file: "custom-templates/image-grids/flex-dest/dev/js/bundle.js",
+    format: "iife",
+    sourcemap: true,
+  },
+  plugins: [
+    nodeResolve({
+      extensions: [".js", ".jsx"],
+    }),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ["@babel/preset-react"],
+    }),
+    commonjs()
+  ]
+};
